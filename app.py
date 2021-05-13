2ed57ad3a53e785248dcca506f131381b09b507f
Denis Senchishen <dsenchishen@icloud.com>
2021-05-13 22:42:47 +0300

    Added calculations

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-import csv
 import json
 import sys
-from typing import Optional
+
 import pandas as pd
+from flask import Flask
 
 INPUT_FILE_PATH = 'data/data.json'
 INPUT_STUDENTS_QUANTITY_PATH = 'data/student_quantity.csv'
+INPUT_DEPARTMENTS_PATH = 'data/departments.csv'
+INPUT_FACULTIES_PATH = 'data/faculties.csv'
+INPUT_SPECIALITIES_PATH = 'data/specialities.csv'
 OUTPUT_FILE_PATH = 'data/result.json'
 
 
@@ -71,6 +74,15 @@ def main(argv):
         students_quantity = pd.read_csv(file)
         students_quantity['Faculty'].fillna(method='ffill', inplace=True)
 
+    with open(INPUT_FACULTIES_PATH, mode='r') as file:
+        faculties = pd.read_csv(file)
+
+    with open(INPUT_DEPARTMENTS_PATH, mode='r') as file:
+        departments = pd.read_csv(file)
+
+    with open(INPUT_SPECIALITIES_PATH, mode='r') as file:
+        specialities = pd.read_csv(file)
+
     # PROCESS DATA
     coefficient = Coefficient(input_data['COEFFICIENT'])
     index = Index(input_data['INDEX'])
@@ -107,8 +119,34 @@ def main(argv):
     faculty_budget = faculty_students_quantity.copy()
     faculty_budget['sum'] = faculty_budget['sum'] * contingent_unit_value
 
-    staff_types = ['NPP', 'NDP', 'AUP']
+    weights_departments = pd.Series([
+        index.DENNA_IR * index.BACHELOR_IF,
+        index.DENNA_IR * index.MASTER_IF,
+        index.ZAOCHNA_IR * index.BACHELOR_IF,
+        index.ZAOCHNA_IR * index.MASTER_IF
+    ], index=['studentsQuantityBachelorFulltime', 'studentsQuantityMasterFulltime',
+              'studentsQuantityBachelorExtramural', 'studentsQuantityMasterExtramural'])
+    specialities['total'] = specialities['index'] * (
+            specialities['studentsQuantityBachelorFulltime'] * weights_departments['studentsQuantityBachelorFulltime'] +
+            specialities['studentsQuantityMasterFulltime'] * weights_departments['studentsQuantityMasterFulltime'] +
+            specialities['studentsQuantityBachelorExtramural'] * weights_departments[
+                'studentsQuantityBachelorExtramural'] +
+            specialities['studentsQuantityMasterExtramural'] * weights_departments['studentsQuantityMasterExtramural'])
 
+    spec_dep = pd.merge(specialities, departments, on="idDepartment")
+    spec_dep_fac = pd.merge(spec_dep, faculties, on="idFaculty")
+
+    spec_dep_fac_totals = spec_dep_fac[['facultyName', 'total']].copy().groupby(['facultyName']).sum().reset_index()
+    faculty_budget['per_coef'] = faculty_budget['sum'] / spec_dep_fac_totals['total']
+    faculty_budget['facultyName'] = faculty_budget['Faculty']
+
+    spec_dep_fac = pd.merge(spec_dep_fac, faculty_budget, how='left', on="facultyName")
+    spec_dep_fac['finance'] = spec_dep_fac['total'] * spec_dep_fac['per_coef']
+
+    result_per_fac = spec_dep_fac[['Faculty', 'finance']].groupby(['Faculty']).sum()
+    result_per_department = spec_dep_fac[['DepartmentName', 'finance']].groupby(['DepartmentName']).sum()
+
+    # staff_types = ['NPP', 'NDP', 'AUP']
     # faculty_staff = faculty_budget[["Faculty", "sum"]].copy()
 
     # faculty_staff = pd.concat([faculty_staff,pd.DataFrame(columns=staff_types)])
@@ -127,11 +165,28 @@ def main(argv):
             "faculty_budget": faculty_budget.to_dict(),
             "university_sum": university_sum,
             "contingent_unit_value": contingent_unit_value,
+            "per_faculty": result_per_fac.to_dict(),
+            "per_department": result_per_department.to_dict(),
             # 'faculty_staff':{
             #     x: faculty_staff[["Faculty", x]].to_dict() for x in staff_types
             # }
         }
         json.dump(result, file, indent=2, ensure_ascii=False)
+        result = json.dumps(result, indent=2, ensure_ascii=False)
+        return result
+
+
+app = Flask(__name__)
+
+
+# $ export FLASK_APP=hello
+# $ flask run
+#  * Running on http://127.0.0.1:5000/
+
+
+@app.route("/")
+def hello_world():
+    return main(sys.argv[1:])
 
 
 if __name__ == "__main__":
